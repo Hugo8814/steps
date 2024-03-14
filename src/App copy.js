f86aaa41e825526cebbfac423acedd075d2e3b14
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-03-14 23:20:22 +0000

    today

--- src/App copy.js
+++ src/App copy.js
@@ -10,6 +10,7 @@ export default function App() {
   return (
     <div>
       <Steps />
+      <Steps />
     </div>
   );
 }
@@ -39,46 +40,28 @@ function Steps() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
           <div className="buttons">
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
+            <button
+              style={{ backgroundColor: "#7950f2", color: "white" }}
               onClick={handlePrevious}
-              text="Previous"
             >
-              <span>👈</span> Previous
-            </Button>
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "white" }}
+              className="next"
               onClick={handleNext}
-              text="Next"
             >
-              Next <span>👉</span>
-            </Button>
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
   );
 }
-function StepMessage({ step, children }) {
-  return (
-    <div className="message">
-      Step {step}: {children}
-    </div>
-  );
-}
 
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      className="next"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-}
+/// test
